68350e67a852ca10bd9e74967aaf2936cd9e427d
Jose Arturo Guzman <125223327+JoseArturoGuzman@users.noreply.github.com>
2024-12-10 17:57:31 -0400

    Initial Commit

---
+++ pruebas_edu/src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import loginImage from "./assets/7fe20f80c5aa73b3f4da483db5ca2276.png"; // Importa la imagen
+import googleImage from "./assets/icons8-google-480.png";
+import intelectoImage from "./assets/2f7c83b3f041fe2e8003c3dc97267813.png";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <>
+      <section className="flex flex-col lg:flex-row min-h-screen justify-center items-stretch">
+        {/* Contenedor del Formulario */}
+        <div className="flex-1 max-w-1/2 p-10 shadow-lg rounded-lg bg-white flex flex-col items-center justify-center">
+          <div className="mb-8 text-center">
+            <img src={intelectoImage} alt="Logo de la Universidad" className="w-60 mx-auto" />
+          </div>
+
+          <form className="w-full max-w-md space-y-6">
+            <div className="w-full">
+              <input
+                type="email"
+                id="email"
+                className="w-full mt-1 p-3 text-lg border border-red-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500"
+                placeholder="✉️0000000@intec.edu.do"
+              />
+            </div>
+
+            <div className="w-full">
+              <input 
+                type="password"
+                id="password"
+                className="w-full mt-1 p-3 text-lg border border-red-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500"
+                placeholder="🔒•••••••••••••••"
+              />
+            </div>
+
+            <div className="flex items-center">
+              <button
+                type="submit"
+                className="w-full py-3 text-white bg-red-600 rounded-full text-lg font-semibold hover:bg-red-700"
+              >
+                Iniciar Sesión
+              </button>
+            </div>
+
+            <div className="text-center mt-4">
+              <a
+                href="/recuperar-contrasena"
+                className="text-red-500 hover:text-red-700 font-semibold text-lg underline"
+              >
+                ¿Has olvidado tu Contraseña?
+              </a>
+            </div>
+
+            <div className="flex items-center justify-center mt-4 text-lg">
+              <a
+                href="/Registro"
+                className="text-red-500 hover:text-red-700 font-semibold underline"
+              >
+                Regístrate
+              </a>
+            </div>
+            <div class="flex items-center w-full">
+  <div class="flex-1 h-0.5 bg-black"></div>
+  <span class="mx-4 text-black text-sm">o</span>
+  <div class="flex-1 h-0.5 bg-black"></div>
+</div>
+
+            <div className=" p-4 flex justify-center">
+              <button className="w-full max-w-sm bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-red-500 rounded-full shadow flex items-center justify-center space-x-3">
+                <img src={googleImage} alt="Google" className="w-8 h-8" />
+                <span className="text-center">
+                  Continúa con tu cuenta de Google
+                </span>
+              </button>
+            </div>
+          </form>
+        </div>
+
+        {/* Contenedor de Imagen */}
+        <div
+          className="flex-1 max-w-1/2 shadow-lg  brightness-75 overflow-hidden flex items-center justify-center bg-cover bg-center "
+          style={{
+            backgroundImage: `url(${loginImage})`,
+          }}
+        ></div>
+      </section>
+    </>
+  );
+}
+
+export default App;
